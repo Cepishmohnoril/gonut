8994117ceb72a88042de5008ac5b83d55187f292
Vladyslav Radchuk <cepishmohnoril@gmail.com>
2023-03-21 14:57:01 +0200

    circle transformed into torus projection

--- main.go
+++ main.go
@@ -8,8 +8,8 @@ import (
 )
 
 const (
-	screenWidth  = 100
-	screenHeight = 100
+	screenWidth  = 400
+	screenHeight = 400
 )
 
 type Game struct {
@@ -19,7 +19,7 @@ type Game struct {
 func main() {
 	g := newGame()
 
-	ebiten.SetWindowSize(screenWidth*8, screenHeight*8)
+	ebiten.SetWindowSize(screenWidth*2, screenHeight*2)
 	ebiten.SetWindowTitle("Render donut in Go. (Gonut) This is not a call. =)")
 
 	if err := ebiten.RunGame(g); err != nil {
@@ -35,12 +35,30 @@ func newGame() *Game {
 	alphaStep := 0.05
 	alphaBorder := math.Pi * 2
 
+	betaStep := 0.1
+	betaBorder := math.Pi * 2
+
+	R1 := 20.0
+	R2 := 5.0
+
+	K2 := 5.0
+	K1 := float64(screenWidth) * K2 * 3.0 / (8.0 * (R1 + R2))
+
 	for alpha := 0.0; alpha <= alphaBorder; alpha += alphaStep {
+		for beta := 0.0; beta <= betaBorder; beta += betaStep {
 
-		x := math.Round(10*math.Cos(alpha)) + 50
-		y := math.Round(10*math.Sin(alpha)) + 50
+			x := (R1 + R2*math.Cos(beta)) * math.Cos(alpha)
+			y := (R1 + R2*math.Cos(beta)) * math.Sin(alpha)
+			z := R2 * math.Sin(beta)
 
-		g.setCell(0xff, x, y)
+			xp := math.Round((K1*x/K2 + z)) + 200
+			yp := math.Round((K1*y/K2 + z)) + 200
+
+			if yp > 0 && xp < screenHeight && xp > 0 && xp < screenWidth {
+
+				g.setCell(0xff, xp, yp)
+			}
+		}
 	}
 
 	return g
